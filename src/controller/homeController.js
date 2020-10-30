a2207a316b7587aad255284670595b641f265647
Brian <blsvsanteliz@gmail.com>
2020-10-30 12:48:41 -0400

    Mixin form

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,40 +1,50 @@
 const Proyecto = require("../models/Proyectos");
 class Home {
   async homeController(req, res) {
-    const proyectos = await Proyecto.findAll();
-    res.render("home", {
-      nombre: "Administra tus proyectos",
-      proyectos,
-    });
-  }
-  async newController(req, res) {
-    const proyectos = await Proyecto.findAll();
-    res.render("new", {
-      nombre: "Nuevo Proyecto",
-      proyectos,
-    });
-  }
-  async newPostController(req, res) {
-    const proyectos = await Proyecto.findAll();
-
-    const { titulo } = req.body;
-
-    let error = [];
-    if (!titulo) {
-      error.push({ error: "Agrega el titulo al proyecto" });
-    }
-    if (error.length > 0) {
-      res.render("new", {
-        nombre: "Nuevo Proyecto",
-        error,
+    try {
+      const proyectos = await Proyecto.findAll();
+      res.render("home", {
+        nombre: "Administra tus proyectos",
         proyectos,
       });
-      return;
+    } catch (error) {
+      console.log(error);
     }
-    await Proyecto.create({
-      titulo,
-    });
-    res.redirect("/");
+  }
+  async newController(req, res) {
+    try {
+      const proyectos = await Proyecto.findAll();
+      res.render("new", {
+        nombre: "Nuevo Proyecto",
+        proyectos,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async newPostController(req, res) {
+    try {
+      const proyectos = await Proyecto.findAll();
+      const { titulo } = req.body;
+      let error = [];
+      if (!titulo) {
+        error.push({ error: "Agrega el titulo al proyecto" });
+      }
+      if (error.length > 0) {
+        res.render("new", {
+          nombre: "Nuevo Proyecto",
+          error,
+          proyectos,
+        });
+        return;
+      }
+      await Proyecto.create({
+        titulo,
+      });
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
   }
   async proyectoGetController(req, res) {
     try {
@@ -47,6 +57,25 @@ class Home {
       if (!proyecto) return res.send("No existe este proyecto");
       res.render("tarea", {
         nombre: "Comienza Agregando Tarea",
+        proyecto,
+        proyectos,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async proyectoGetEditarController(req, res) {
+    try {
+      const { id } = req.params;
+      const [proyecto, proyectos] = await Promise.all([
+        Proyecto.findOne({
+          where: { id },
+        }),
+        Proyecto.findAll(),
+      ]);
+      res.render("new", {
+        nombre: `Editar Proyecto - ${proyecto.titulo}`,
         proyecto,
         proyectos,
       });
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ const Home = require("../controller/homeController");
 const { body } = require("express-validator");
 const controller = new Home();
 const router = Router();
+
+//Ruta para home
 router.get("/", controller.homeController);
 router.get("/new", controller.newController);
 router.post(
@@ -10,5 +12,8 @@ router.post(
   [body("titulo").not().isEmpty().trim().escape()],
   controller.newPostController
 );
+
+//Rutas para los proyectos
 router.get("/proyecto/:url", controller.proyectoGetController);
+router.get("/proyecto/editar/:id", controller.proyectoGetEditarController);
 module.exports = router;
